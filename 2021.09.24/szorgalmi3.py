dec70f79b4722509b2032a297caf9037ad60198e
barnus07 <barnus07@gamma.ttk.pte.hu>
2021-09-24 16:35:21 +0200

    done

--- 2021.09.24/szorgalmi3.py
+++ 2021.09.24/szorgalmi3.py
@@ -11,11 +11,11 @@ if ido == 0:
     print("Now")
 if ido < 60:
     print(ido, "seconds")
-if ido > 59 and ido < 3600:
+if ido > 59 and ido < (60*60):
     print(perc, "minutes and", ido%60, "seconds")
-if ido > 3599 and ido < 86400 :
+if ido > ((60*60)-1) and ido < (60*60*24):
     print(ora, "hour,", perc%60, "minutes","and", ido%60, "seconds")
-if ido > 86399 and ido < 31536000:
+if ido > ((60*60*24)-1) and ido < (60*60*24*365):
     print(nap, "day", ora%24, "hour,", perc%60, "minutes", "and", ido%60, "seconds")
-if ido > 31535999:
+if ido > ((60*60*24*365)-1):
     print(ev, "year,", nap%365, "day,", ora%24, "hour,", perc%60, "minutes and", ido%60, "seconds")
