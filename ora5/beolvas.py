0e04459f37a43ac8b2bc18cd56cc8db5d5b1d80e
barnus0705 <barnabaskaroly@gmail.com>
2021-10-10 12:06:12 +0200

    done

--- ora5/beolvas.py
+++ ora5/beolvas.py
@@ -7,10 +7,24 @@ with open("szamok.txt", "rt" , encoding="UTF-8") as fajl:
         Eszam.append(sor.split(".")[0])
         Mszam.append(sor.split(".")[1])
 
-    i = int(input("Adja meg az indexet!:"))
-    print(Eszam[i],Mszam[i])
+    sorvege = len(Eszam)
+    i = int(input(f'Adja meg az indexet!(1-{sorvege})között:'))
+    print(Eszam[i-1],Mszam[i-1])
+    i = 0
+    count = 1
+    while 1 < 10:
+        print(f'{count}.',Eszam[i], Mszam[i])
+        i += 1
+        count += 1
+        if i == 10:
+            break
 
+    i = str(input("adja meg az első számot:"))
+    sorszam = Eszam.index(i)
+    print("Az első szám párja a",f'{Mszam[sorszam]}.')
 
+    i = 0
 
-
-
+    i = str(input("adja meg az második számot:"))
+    sorszam = Mszam.index(i)
+    print("Az Második szám párja a", f'{Eszam[sorszam]}.')
