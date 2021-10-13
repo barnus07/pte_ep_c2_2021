165d1e32c461f6b4a76f679fd488186740f40c88
barnus0705 <barnabaskaroly@gmail.com>
2021-10-13 19:05:22 +0200

    done

--- Hf4/Hf4.py
+++ Hf4/Hf4.py
@@ -8,4 +8,9 @@ for i in range(1, 11):
     print()
 print("2.feladat")
 print("3.feladat")
+nsor = []
+n = int(input("Adja meg az N sort:"))
+for i in range(1,n+1):
+    nsor.append(math.factorial(n)/(math.factorial(i)*(math.factorial(n-i))))
+print(nsor)
 print("4.feladat")
