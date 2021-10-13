f118ec40a3820408ae2c8c6435d4a582524a847b
barnus0705 <barnabaskaroly@gmail.com>
2021-10-13 18:44:21 +0200

    done

---
+++ Hf4/Hf4.py
@@ -0,0 +1,11 @@
+import math
+
+print("1.feladat")
+for i in range(1, 11):
+    for y in range(1, 11):
+        print('{:3}'.format(i*y), end=' ')
+
+    print()
+print("2.feladat")
+print("3.feladat")
+print("4.feladat")
