9f87890cc532399c41a086479981d7a1295c6e19
barnus0705 <barnabaskaroly@gmail.com>
2021-11-20 14:15:51 +0100

    d

---
+++ hf9/feladat1.py
@@ -0,0 +1,13 @@
+szoveg = ""
+def equal(szoveg):
+    for i in range(5):
+        szoveg = szoveg + "a"
+    for i in range(16):
+        szoveg = szoveg + "x"
+    for i in range(9):
+        szoveg = szoveg + "p"
+    print(szoveg)
+equal(szoveg)
+
+
+
