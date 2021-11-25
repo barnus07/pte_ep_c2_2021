81e101230fe667cf8686d06d83cc06fc85c51999
barna <barnus07@gamma.pte.hu>
2021-11-25 18:18:55 +0100

    done

--- hf10/feladat1.py
+++ hf10/feladat1.py
@@ -1,13 +1,25 @@
+
+def input(users: dict):
+    for i in range(2):
+        name = input("Adjon meg egy nevet:\t")
+        phone_numbers = input("Adjon meg egy telefonszámot:\t")
+        answer = input("Szeretne még megadni telefonásmot?(igen/nem)\n")
+        if answer == "igen":
+            phone_numbers2 = input("Adjon meg egy telefonszámot:\t")
+            users[name] = phone_numbers,phone_numbers2
+        else:
+            users[name] = phone_numbers
+def ABC(users: dict):
+    for key,value in sorted(users.items()):
+        sorted(users.keys())
+        print(key,value)
+
+def require(users: dict):
+    print(users.keys())
+    name = input("adjon meg egy nevet a fentiekkből: \t")
+    print(users[name])
+
 users = {}
-for i in range(2):
-    name = input("Adjon meg egy nevet:\t")
-    phone_numbers = input("Adjon meg egy telefonszámot:\t")
-    answer = input("Szeretne még megadni telefonásmot?(igen/nem)\n")
-    if answer == "igen":
-        phone_numbers2 = input("Adjon meg egy telefonszámot:\t")
-        users[name] = phone_numbers,phone_numbers2
-    else:
-        users[name] = phone_numbers
-print("A lista nevei:\n",list(users))
-name = input("adjon meg egy nevet a listából:\t")
-print(users.get(name))
+input(users)
+ABC(users)
+require(users)
