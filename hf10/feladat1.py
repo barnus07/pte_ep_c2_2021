fbb206fb7be55a8e2da6a695d79e293347ba70a0
barna <barnus07@gamma.pte.hu>
2021-11-26 12:28:47 +0100

    done

--- hf10/feladat1.py
+++ hf10/feladat1.py
@@ -1,21 +1,19 @@
-
 def input(users: dict):
     for i in range(2):
-        name = input("Adjon meg egy nevet:\t")
-        phone_numbers = input("Adjon meg egy telefonszámot:\t")
-        answer = input("Szeretne még megadni telefonásmot?(igen/nem)\n")
-        if answer == "igen":
-            phone_numbers2 = input("Adjon meg egy telefonszámot:\t")
-            users[name] = phone_numbers,phone_numbers2
-        else:
-            users[name] = phone_numbers
-
+         name = input("Adjon meg egy nevet:\t")
+            phone_numbers = input("Adjon meg egy telefonszámot:\t")
+            answer = input("Szeretne még megadni telefonásmot?(igen/nem)\n")
+            if answer == "igen":
+                phone_numbers2 = input("Adjon meg egy telefonszámot:\t")
+                users[name] = phone_numbers,phone_numbers2
+            else:
+                users[name] = phone_numbers
 def ABC(users: dict):
     for key,value in sorted(users.items()):
         sorted(users.keys())
         print(key,value)
 
-def require(users: dict):
+def find(user: dict):
     print(users.keys())
     name = input("adjon meg egy nevet a fentiekkből:\t")
     print(users[name])
@@ -23,4 +21,4 @@ def require(users: dict):
 users = {}
 input(users)
 ABC(users)
-require(users)
+find(users)
