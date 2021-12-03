850704bee040376105074711b3b434a71b16920b
barna <barnus07@gamma.pte.hu>
2021-12-03 08:22:59 +0100

    done

---
+++ gyak/MAIN_ZH_02.py
@@ -0,0 +1,12 @@
+import proba_zh2_gyak
+import  kepviselo
+
+lista = proba_zh2_gyak.beolvasas()
+lista2 = proba_zh2_gyak.osztaly(lista)
+print("1.feladat: file beolvasva")
+proba_zh2_gyak.masodik(lista2)
+proba_zh2_gyak.harmadik(lista2)
+proba_zh2_gyak.negyedik(lista2)
+proba_zh2_gyak.otodik(lista2)
+proba_zh2_gyak.hatodik(lista2)
+proba_zh2_gyak.hetedik(lista2)
---
+++ gyak/kepviselo_02.py
@@ -0,0 +1,13 @@
+class Kepviselo:
+
+    """A képviselő osztály megadása, jellemzése"""
+
+    def __init__(self,ker: int, votes: int, nev1:str , nev2:str, rovpart: str,part: str):
+        self.ker = ker
+        self.votes = votes
+        self.nev1 = nev1
+        self.nev2 = nev2
+        self.part = part
+        self.rovpart =rovpart
+
+
